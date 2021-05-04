1f49ebb555e7bbb39f2077fe65f7bcb52281564e
Paul de Mast <pjf.demast@avans.nl>
2021-05-04 16:00:29 +0200

    Add project files.

---
+++ News/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace News
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                // todo create your own News api key
+                //return "142b30001ea949fa8cb10e0f7cb4c463";
+                return "2ec46eb5b219463e9d7fb94955ff39bc";
+            }
+        }
+    }
+}
